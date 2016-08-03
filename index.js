8af495892e1d87931177fbae3343d1591e38430a
RangelZZZ <18792561236@163.com>
2016-08-03 23:10:52 +0800

    button can switch

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
+'use strict';
+
 const App = React.createClass({
 
+    getInitialState: function () {
+        return {
+            isEditor: true,
+            elements: []
+        }
+    },
+
+    taggle: function () {
+        this.setState({
+            isEditor: !this.state.isEditor
+        });
+    },
+
+    render: function () {
+        const isEditor = this.state.isEditor;
+        return <div>
+            <button onClick={this.taggle}>{isEditor ? "Preview" : "Edit"}</button>
+            <div className={isEditor ? 'hidden' : ''}>
+                <Editor/>
+            </div>
+            <div className={isEditor ? '' : 'hidden'}>
+                <Preview/>
+            </div>
+        </div>
+    }
+});
+
+const Editor = React.createClass({
     render: function () {
         return <div>
-            hello !
+            hello
+        </div>
+    }
+});
+
+const Preview = React.createClass({
+    render: function () {
+        return <div>
+            world
         </div>
     }
 });
