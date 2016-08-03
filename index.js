523ba01dcaa5228384e2f9d208841a680bc5362a
RangelZZZ <18792561236@163.com>
2016-08-04 00:06:15 +0800

    add elements

--- index.js
+++ index.js
@@ -15,14 +15,20 @@ const App = React.createClass({
         });
     },
 
+    addElement: function (element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
+
     render: function () {
         const isEditor = this.state.isEditor;
         return <div>
-            <button onClick={this.taggle}>{isEditor ? "Preview" : "Edit"}</button>
-            <div className={isEditor ? 'hidden' : ''}>
-                <Editor/>
-            </div>
+            <button onClick={this.taggle}>{isEditor ? "Edit" : "Preview"}</button>
             <div className={isEditor ? '' : 'hidden'}>
+                <Editor elements={this.state.elements} onAdd={this.addElement}/>
+            </div>
+            <div className={isEditor ? 'hidden':'' }>
                 <Preview/>
             </div>
         </div>
@@ -32,7 +38,40 @@ const App = React.createClass({
 const Editor = React.createClass({
     render: function () {
         return <div>
-            hello
+            <Left elements={this.props.elements}/>
+            <Right onAdd={this.props.onAdd}/>
+        </div>
+    }
+});
+
+const Left = React.createClass({
+    render: function () {
+
+        const elements = this.props.elements.map((ele, index)=> {
+            return <div key={index}>
+                <input type={ele}/>
+                <button>X</button>
+            </div>
+        });
+
+        return <div>
+            {elements}
+        </div>
+    }
+});
+
+const Right = React.createClass({
+
+    add: function () {
+        const element = $("input[name=element]:checked").val();
+        this.props.onAdd(element);
+    },
+
+    render: function () {
+        return <div>
+            <input type="radio" name="element" value="text"/>Text
+            <input type="radio" name="element" value="date"/>date
+            <button onClick={this.add}>+</button>
         </div>
     }
 });
@@ -45,4 +84,6 @@ const Preview = React.createClass({
     }
 });
 
-ReactDOM.render(<App/>, document.getElementById("content"));
+ReactDOM.render(
+    <App/>
+    , document.getElementById("content"));
